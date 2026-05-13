296b4ac7e3b4a9c3af7de831673f30691dafd81e
Elias <144105283+EviV2@users.noreply.github.com>
2026-05-13 10:40:57 +0200

    Feat: caché le boutton admin quand l'on est pas

--- app/middleware/admin.js
+++ app/middleware/admin.js
@@ -2,13 +2,10 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const JWT_PEPPER = process.env.JWT_PEPPER;
-
 const verifyAdmin = (req, res, next) => {
   //req.user pour prévenir d'un crash  (aukaou)
   if (req.user && req.user.role !== "admin") {
-    return res.redirect("/");
-    //return res.status(403).json({ error: "Access refused. Admins only." });
+    return res.status(403).json({ error: "Access refused. Admins only." });
   }
   next();
 };
--- app/public/js/nav.js
+++ app/public/js/nav.js
@@ -1,8 +1,21 @@
 // Navigation commune à toutes les pages
 // Pour modifier le menu, éditer uniquement ce fichier
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const nav = document.getElementById("topbar");
+
   if (!nav) return;
+  let adminPath = "";
+  try {
+    const response = await fetch("/api/auth/status");
+    if (response.ok) {
+      const data = await response.json();
+      if (data.role === "admin") {
+        adminPath = `<a href="/admin">Admin</a>`;
+      }
+    }
+  } catch (error) {
+    console.error("Non connecté ou erreur de vérification");
+  }
   nav.innerHTML = `
         <header class="topbar">
             <div class="container">
@@ -10,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <nav class="menu">
                     <a href="/">Accueil</a>
                     <a href="/profile">Profil</a>
-                    <a href="/admin">Admin</a>
+                    ${adminPath}
                     <a href="/login">Connexion</a>
                     <a href="/register">Inscription</a>
                 </nav>
